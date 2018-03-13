e412df55d25226e0919c05f996d6d0322b411b81
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2018-03-13 10:39:56 +0100

    Camera able to capture photo.

--- src/components/camera/camera.tsx
+++ src/components/camera/camera.tsx
@@ -1,20 +1,31 @@
 import * as React from 'react';
 import {ReactNode} from 'react';
 
-export default class Camera extends React.Component {
+export interface CameraProps {}
+export interface CameraState {
+    displayBtn: boolean;
+}
 
-    private video: any = null;
+export default class Camera extends React.Component<CameraProps, CameraState> {
 
-    constructor(props: {}) {
+    private _video: any;
+    private _canvas: any;
+
+    constructor(props: CameraProps) {
         super(props);
+        this.state = {displayBtn: false};
+
         this.onCameraClick = this.onCameraClick.bind(this);
+        this.onUpload = this.onUpload.bind(this);
         this.handleSuccess = this.handleSuccess.bind(this);
     }
 
     private handleSuccess(stream: MediaStream): void {
         console.log('success', stream);
-        console.log('video', this.video);
-        this.video.srcObject = stream;
+        console.log('_video', this._video);
+        console.log(this._video.constructor.name);
+        this._video.srcObject = stream;
+        this.setState({displayBtn: true});
     }
 
     private handleError(error: any): void {
@@ -22,14 +33,24 @@ export default class Camera extends React.Component {
     }
 
     private onCameraClick(event: any): void {
-        console.log('onCameraClick', event);
         if (this.hasGetUserMedia()) {
-            console.log('onCameraClick. has get user media');
-            navigator.mediaDevices.getUserMedia({video: true}).
+
+            const hdConstraints = {
+                video: {width: {min: 1280}, height: {min: 720}}
+            };
+
+            navigator.mediaDevices.getUserMedia(hdConstraints).
             then(this.handleSuccess).catch(this.handleError);
         } else {
             console.error('getUserMedia() is not supported by your browser');
         }
+    }
+
+    private onUpload(event: any): void {
+        this._canvas.width = this._video.videoWidth;
+        this._canvas.height = this._video.videoHeight;
+        this._canvas.getContext('2d').drawImage(this._video, 0, 0);
+
     }
 
     private hasGetUserMedia(): boolean {
@@ -40,9 +61,9 @@ export default class Camera extends React.Component {
         return <div className='camera'>
             <button id='camera' onClick={this.onCameraClick} />
             <label htmlFor='camera'>Click the camera icon!</label>
-            <video autoPlay ref={(vid) => {this.video = vid; }} />
-            <img src='' />
-            <canvas />
+            <video autoPlay ref={(vid) => {this._video = vid; }} />
+            <canvas ref={(c) => {this._canvas = c; }} />
+            <button id='capture-button' className={this.state.displayBtn ? 'display' : ''} onClick={this.onUpload}>Capture</button>
         </div>;
     }
 
